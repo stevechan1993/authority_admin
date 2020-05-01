08b5c2a2b73d2bdf30737350c154bb93b153de16
Steve Chan <zhiying.chen@formal-tech.com>
2020-05-01 16:23:28 +0800

    first commit

---
+++ src/main/java/com/steve/demo/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.steve.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping(value="/hello")
+    public Object hello() {
+        return "Hello Demo!";
+    }
+}
