a203122cd76d66dc2872c7278206629e72b17cc0
Steve Chan <steve.d.chan@qq.com>
2020-06-24 17:44:35 +0800

    rename packages

--- src/main/java/com/steve/authority/AuthorityApplication.java
+++ src/main/java/com/steve/authority/AuthorityApplication.java
@@ -1,13 +1,13 @@
-package com.steve.demo;
+package com.steve.authority;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class AuthorityApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(AuthorityApplication.class, args);
 	}
 
 }
